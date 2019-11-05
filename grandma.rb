78f8a6e90c68bd9165b349cb5206c3ca7cd18471
Jeremy Bourne <jbourne25@yahoo.com>
2019-11-05 23:20:44 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,7 @@ def speak_to_grandma(phrase)
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase.upcase
     return "NO, NOT SINCE 1938!"
-  else phrase == "I LOVE YOU GRANDMA!"
+  else phrase == phrase
     return "LOVE YOU TOO PUMKIN!"
   end
 end
