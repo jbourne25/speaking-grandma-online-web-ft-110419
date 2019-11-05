fd0c681e1c1b57a35536773759699cec736015e5
Jeremy Bourne <jbourne25@yahoo.com>
2019-11-05 23:26:59 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 
 def speak_to_grandma(phrase)
-  phrase == "I LOVE YOU GRANDMA!"
+  #phrase == "I LOVE YOU GRANDMA!"
   if phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
   else phrase.upcase
