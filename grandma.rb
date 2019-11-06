95f937fe7ef70eedb3099c41682e6e215a7ebb07
Jeremy Bourne <jbourne25@yahoo.com>
2019-11-06 00:08:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,10 +18,10 @@ def speak_to_grandma(phrase)
     return "HUH?! SPEAK UP, SONNY!"
   #elsif phrase == phrase.downcase
    # return "HUH?! SPEAK UP, SONNY!"
-  #elsif phrase == phrase.upcase()
-   # return "NO, NOT SINCE 1938!"
-  else 
+  else phrase == phrase.upcase()
     return "NO, NOT SINCE 1938!"
+ # else 
+   # return "NO, NOT SINCE 1938!"
   end
 end 
  
